8b93fe77e223118fc5c902db0495799af937ee66
ashrafkabir2001 <ashraful.kabir@welldev.io>
2024-02-28 11:20:32 +0600

    feat: edit task feature done

--- js/script.js
+++ js/script.js
@@ -39,16 +39,68 @@ const deleteTaskHandler = (event) => {
     taskList = taskList.filter((task) => task.id!=taskId);
 };
 
+const saveTaskEditHandler = (task, taskDetails, inputField, editButton, saveButton, cancelButton) => {
+    task.title = inputField.value;
+    
+    taskList.map((t) => {
+        if(t.id==task.id){
+            return task;
+        }
+        return t;
+    });
+
+    taskDetails.innerHTML = `${task.title}, ${task.createdAt} `;
+
+    toggleElements(taskDetails, inputField, editButton, saveButton, cancelButton);
+}
+
+const toggleElements = (taskDetails, inputField, editButton, saveButton, cancelButton) => {
+    taskDetails.hidden = !taskDetails.hidden;
+    editButton.hidden = !editButton.hidden;
+    inputField.hidden = !inputField.hidden;
+    saveButton.hidden = !saveButton.hidden;
+    cancelButton.hidden = !cancelButton.hidden;
+}
+
 const createTaskElement = (task) => {
     const taskElement = document.createElement("li");
+    const inputField = document.createElement("input");
+    const taskDetails = document.createElement("span");
     const deleteButton = document.createElement("button");
-
-    deleteButton.innerText = "Delete";
-    deleteButton.addEventListener("click", (event) => deleteTaskHandler(event));
+    const editButton = document.createElement("button");
+    const saveButton = document.createElement("button");
+    const cancelButton = document.createElement("button");
 
     taskElement.id = task.id;
-    taskElement.innerHTML = `${task.title}, ${task.createdAt} `;
+    
+    taskDetails.innerHTML = `${task.title}, ${task.createdAt} `;
+    
+    inputField.value = `${task.title}`;
+    inputField.hidden = true;
+    
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", (event) => deleteTaskHandler(event));
+    
+    editButton.innerHTML = "Edit";
+    editButton.addEventListener("click", () => toggleElements(taskDetails, inputField, editButton, saveButton, cancelButton));
+    
+    saveButton.innerHTML = "Save";
+    saveButton.addEventListener("click", () => saveTaskEditHandler(task, taskDetails, inputField, editButton, saveButton, cancelButton));
+    saveButton.hidden = true;
+    
+    cancelButton.innerHTML = "Cancel";
+    cancelButton.addEventListener("click", () => {
+        inputField.value = task.title;
+        toggleElements(taskDetails, inputField, editButton, saveButton, cancelButton);
+    });
+    cancelButton.hidden = true;
+
+    taskElement.appendChild(taskDetails);
+    taskElement.appendChild(inputField);
     taskElement.appendChild(deleteButton);
+    taskElement.appendChild(editButton);
+    taskElement.appendChild(saveButton);
+    taskElement.appendChild(cancelButton);
 
     return taskElement;
 };
