944fcb42301597cfea36de1022f62674210bc5d9
ashrafkabir2001 <ashraful.kabir@welldev.io>
2024-03-12 14:39:39 +0600

    refactor: move const variables to const.js

--- js/const.js
+++ js/const.js
@@ -3,3 +3,6 @@ export const reg = /[&<>"'/`]/gi;
 export const ALL = "all";
 export const INCOMPLETE = "incomplete";
 export const COMPLETED = "completed";
+
+const START_INDEX = 0;
+const TASK_PER_PAGE = 9;
